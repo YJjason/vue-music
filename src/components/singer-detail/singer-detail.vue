b06e7f74718cf0b2bc77c8559e50f101fce830be
myj <2908037646@qq.com>
2019-07-23 17:39:12 +0800

    歌手详情接口

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,35 +1,77 @@
 <template>
   <transition
-  name="slide"
+    name="slide"
   >
     <div class="singer-detail">
-
-  </div>
+      <ul>
+        <li v-for="item in songs">
+         <span> {{item.musicData.songname}}</span>
+        </li>
+      </ul>
+    </div>
 
   </transition>
 </template>
 
 <script>
-export default {
+  import {getSingerDetail} from '../../api/singer'
+  import {createSong} from '../../common/js/song'
+  import {ERR_OK} from "../../api/config";
+
+  export default {
+    data() {
+      return {
+        songs: []
+      }
+    },
+    created() {
+      this._getDetail()
+    },
+    methods:{
+      _getDetail(){
+       /* if(!this.singer.id){
+          this.$router.push('/signer')
+          return
+        }*/
+        getSingerDetail('002J4UUk29y8BY').then(res=>{
+          console.log(23,res)
+          if(res.code===ERR_OK){
+            // this.songs=this._normalizeSongs(res.data.list)
+            this.songs=res.data.list
+          }
+        })
+      },
+      _normalizeSongs(list){
+        let ret =[]
+        list.forEach(item=>{
+          let {musicData} =item
+          if(musicData.id&&musicData.albumid){
+            ret.push(createSong(musicData))
+          }
+        })
+      }
+    }
 
 
-}
+  }
 </script>
 <style lang="stylus" scoped rel="stylesheet/stylus">
-@import '../../common/stylus/variable';
+  @import '../../common/stylus/variable';
 
-.singer-detail
-  position: fixed
-  z-index:100
-  top:0
-  left:0
-  right:0
-  bottom:0
-  background :$color-background
-.slide-enter-active,slide-leave-active
-  transition :all 0.3s
-.slide-enter,.slide-leave-to
-  transform  :translate3d(100%,0,0)
+  .singer-detail
+    position: fixed
+    z-index: 100
+    top: 0
+    left: 0
+    right: 0
+    bottom: 0
+    background: $color-background
+
+  .slide-enter-active, slide-leave-active
+    transition: all 0.3s
+
+  .slide-enter, .slide-leave-to
+    transform: translate3d(100%, 0, 0)
 
 </style>
 
--- src/store/getters.js
+++ src/store/getters.js
@@ -1,1 +1,1 @@
-
+export const singer = state => state.singer
