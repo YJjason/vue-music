a9c1b3e11e852cb097111f7931e4682ad42f7909
YJjason <1009239228@qq.com>
2019-08-13 22:54:48 +0800

    歌单详情页布局

---
+++ src/components/disc/disc.vue
@@ -0,0 +1,27 @@
+<template>
+  <transition name="slide">
+    <music-list></music-list>
+  </transition>
+</template>
+
+<script>
+
+  import MusicList from '../music-list/music-list'
+
+  export default {
+    name: "disc",
+    components: {
+      MusicList
+    },
+
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .slide-enter-active, .slide-leave-active
+    transition: all 0.3
+
+  .slide-enter, .slide-leave-to
+    transform: translate3d(100%, 0, 0)
+
+</style>
